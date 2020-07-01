6a30f760a5e802e29a0ac9a8a3b05f3829bf7201
Gregory A.S. Wright <geslim@gmail.com>
2020-07-01 16:53:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 def reverse_each_word(string)
- string.collect do
-   reverse_string = string.reverse
-   split_reverse_string = reverse_string.split
-   reverse_split_reversed_string = split_reverse_string.reverse
-   reverse_split_reversed_string.join(" ")
+ string.collect do |element|
+ element
  end
 end
 
