b8ce4355e2589f2410354c9c40446ccd7311707d
Gregory A.S. Wright <geslim@gmail.com>
2020-07-01 17:24:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   split_string.collect do |element|
     reverse_string << element.reverse
   end
-  reverse_string
+  reverse_string.join(" ")
 end
 
 #reverse_string = string.reverse
