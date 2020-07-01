cf8265e1cb9984381db687e7b2c4bea3d06e0d5b
Gregory A.S. Wright <geslim@gmail.com>
2020-07-01 16:49:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(string)
   reverse_string = string.reverse
   split_reverse_string = reverse_string.split
   reverse_split_reversed_string = split_reverse_string.reverse
-  reverse_split_reversed_string.join
+  reverse_split_reversed_string.join(" ")
 
 end
