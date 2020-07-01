4a38c21e35527a9b9ccf91c5c2d800d135f88faa
Gregory A.S. Wright <geslim@gmail.com>
2020-07-01 16:52:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,13 @@
 def reverse_each_word(string)
-  reverse_string = string.reverse
-  split_reverse_string = reverse_string.split
-  reverse_split_reversed_string = split_reverse_string.reverse
-  reverse_split_reversed_string.join(" ")
-
+ string.collect do 
+   reverse_string = string.reverse
+   split_reverse_string = reverse_string.split
+   reverse_split_reversed_string = split_reverse_string.reverse
+   reverse_split_reversed_string.join(" ")
+ end
 end
+
+#reverse_string = string.reverse
+#split_reverse_string = reverse_string.split
+#reverse_split_reversed_string = split_reverse_string.reverse
+#reverse_split_reversed_string.join(" ")
